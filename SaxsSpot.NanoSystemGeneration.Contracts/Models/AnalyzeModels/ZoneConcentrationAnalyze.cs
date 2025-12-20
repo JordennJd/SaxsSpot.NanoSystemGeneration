100b1954f1289808c64b9bb7825d0699465bc765
Danil <112797010+JordennJd@users.noreply.github.com>
2025-12-20 09:39:38 +0300

    feat(analysis): add fromString and toString methods

--- SaxsSpot.NanoSystemGeneration.Contracts/Models/AnalyzeModels/ZoneConcentrationAnalyze.cs
+++ SaxsSpot.NanoSystemGeneration.Contracts/Models/AnalyzeModels/ZoneConcentrationAnalyze.cs
@@ -1,3 +1,19 @@
+using System.Globalization;
+
 namespace SaxsSpot.NanoSystemGeneration.Contracts.Models.AnalyzeModels;
 
-public record ZoneConcentrationAnalyze(int ZoneIndex, double Concentration);
+public record ZoneConcentrationAnalyze(int ZoneIndex, double Concentration)
+{
+    public static ZoneConcentrationAnalyze FromString(string rad)
+    {
+        var parameters = rad.Split().Select(x => float.Parse(x, NumberFormatInfo.InvariantInfo)).ToList();
+
+        return new ZoneConcentrationAnalyze(float.ConvertToInteger<int>(parameters[0]), parameters[1]);
+    }
+    
+    public override string ToString()
+    {
+        return string.Format(CultureInfo.InvariantCulture, 
+            "{0} {1}", ZoneIndex, Concentration);
+    }
+}
